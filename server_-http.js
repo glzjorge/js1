12f7be117f813cd3388d9fef5a11bb08bdae2182
Jorge Luis Gonzalez Hernandez <jorgegh@delicias.tecnm.mx>
2023-10-06 09:19:27 -0600

    cambiamos serverjs con texto en html

--- server_-http.js
+++ server_-http.js
@@ -1,11 +1,11 @@
 const http = require('http');
-const hostname = '172.16.7.30';
+const hostname = '192.168.1.143';
 const port = 3000;
 
 const server = http.createServer((req,res)=> {
     res.statusCode = 200;
-    res.setHeader ('Content-Type', 'text/plain');
-    res.end('Hola mundo\n');
+    res.setHeader ('Content-Type', 'text/html');
+    res.end('<h1>Hola mundo\n</h1>');
 
 });
 
